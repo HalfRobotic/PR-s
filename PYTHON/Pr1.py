15d7d53d14dc323f59b044789f07125691664778
Half Robotic <halfrobotic@gmail.com>
2023-05-25 19:45:20 +0200

    Organizando

--- PYTHON/Pr1.py
+++ PYTHON/Pr1.py
@@ -3,5 +3,5 @@ IGI = preu*4.5/100
 preu_final = preu + IGI
 
 
-print("El preu sense impostos es: ",preu, " €")
-print("El preu amb els impostos es: ",preu_final, " €" )
+print("El preu sense impostos es: ", preu, " €")
+print("El preu amb els impostos es: ", preu_final, " €")
